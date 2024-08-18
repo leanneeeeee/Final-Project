24e19e545346cb54a484ef37cf3994062b5ec699
leanneeeeee <84019672+leanneeeeee@users.noreply.github.com>
2024-08-18 11:07:14 +0800

    Modify Player file

---
+++ src/main/scala/fp/game/pvz/model/Player.scala
@@ -0,0 +1,64 @@
+package fp.game.pvz.model
+
+import scalafx.beans.property.{IntegerProperty, StringProperty, ObjectProperty}
+import scalikejdbc._
+import fp.game.pvz.util.Database
+import scala.util.{ Try, Success, Failure }
+
+
+class Player(_name: String, _coins: Int, _level: String) {
+  val name: StringProperty = new StringProperty(_name)
+  val coins: StringProperty = new IntegerProperty(_coins)
+  val level: StringProperty = new StringProperty(_level)
+
+  def save(): Try[Int] = {
+    Try(DB autoCommit { implicit session =>
+      sql"""
+           insert into record (
+                               name,
+                               coins,
+                               level
+                               ) values
+                                        (
+                                         ${name.value},
+                                         ${coins.value},
+                                         ${level.value}
+                                         )
+         """.update.apply()
+    })
+  }
+
+}
+
+
+object Player extends Database{
+  val empty = new Player("", 0, null)
+
+  def initializeTable(): Boolean = {
+    DB autoCommit { implicit session =>
+      sql"""
+           create table player (
+               id int not null GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+               name varchar(255),
+               coins int,
+               level varchar(255)
+           )
+
+         """.execute.apply()
+    }
+  }
+
+  def getAllPlayer(count: Int): List[Player] = {
+    DB readOnly{implicit session =>
+      sql"select * from record order by score desc fetch first ${count} rows only".map(rs => {
+        new Player(
+          rs.string("name"),
+          rs.int("coins"),
+          rs.string("level")
+        )
+      }).list.apply()
+    }
+  }
+
+}
+
